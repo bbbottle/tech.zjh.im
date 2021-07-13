ad8065565dbfc937380fca0a96ff42f44c73a038
zjhou <z@zjh.im>
2021-07-13 07:23:33 +0000

    init commit

---
+++ theme.config.js
@@ -0,0 +1,9 @@
+// theme.config.js
+export default {
+  footer: <p>MIT 2021 ©zjh.im</p>,
+  head: <>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="description" content="Nextra: the next blog builder" />
+    <meta name="og:title" content="Nextra: the next blog builder" />
+  </>
+}
