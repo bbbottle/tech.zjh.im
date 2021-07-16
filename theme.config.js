6a3081b4b0179ae4d645ea025aa8414302c3f69a
zjhou <z@zjh.im>
2021-07-17 05:37:37 +0800

    chore(theme.config.js): add emoji favicon

--- theme.config.js
+++ theme.config.js
@@ -2,6 +2,7 @@
 export default {
   footer: <small style={{ display: 'block', marginTop: '8rem' }}>MIT 2021 ©zjh.im</small>,
   head: <>
+    <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>&#128221;</text></svg>" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta name="description" content="Nextra: the next blog builder" />
     <meta name="og:title" content="Nextra: the next blog builder" />
