4234262d32cb26f7f0a87f98d39417d825c8db1b
zjhou <z@zjh.im>
2021-07-19 17:08:56 +0800

    feat(components/table): new component

---
+++ components/Table.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+type TableProps = {
+    headsStringMatrix: string[][],
+    bodyStringMatrix: string[][],
+}
+
+const Table = (props) => {
+    const {
+        headsStringMatrix,
+        bodyStringMatrix,
+    } = props;
+
+    return (
+        <table>
+            <thead>
+                {headsStringMatrix.map(row => <tr>{row.map(col => <th>{col}</th>)}</tr>)}
+            </thead>
+            <tbody>
+                {bodyStringMatrix.map(row => <tr>{row.map(col => <td>{col}</td>)}</tr>)}
+            </tbody>
+        </table>
+    )
+}
+
+export default Table;
